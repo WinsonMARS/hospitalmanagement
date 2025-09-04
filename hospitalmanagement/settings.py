64f718c44ab0476b0ca4db5248fe3ac7de5f3be3
TofuSoySauce <adrinyongyikai@gmail.com>
2025-09-04 18:43:51 +0800

    Modify setting.py: add STATIC_ROOT

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -129,6 +129,9 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[STATIC_DIR,]
 
+# STATIC_ROOT is the directory where collectstatic will gather all static files for production (and for Docker builds).
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # added this line
+
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 
 
